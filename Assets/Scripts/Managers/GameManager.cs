6857537e5066bc5aed2d4e1c5a4bfbf187808213
Rafa Vital <2165692-rafavital@users.noreply.gitlab.com>
2023-09-20 13:05:38 -0300

    feat(GameManager): creates GameManager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BRK.Events;
+
+namespace BRK.Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+
+    }
+}
