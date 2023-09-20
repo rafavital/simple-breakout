91677aa4403809f90e4f31d6b667c2f1a28ca4f1
Rafa Vital <2165692-rafavital@users.noreply.gitlab.com>
2023-09-20 13:33:40 -0300

    feat(level): adds level generation

--- Assets/Scripts/Managers/LevelManager.cs
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Vector2 m_gridSize = new Vector2(10, 10);
+    [SerializeField] private Vector2 m_padding = new Vector2(0.25f, 0.25f);
+    [SerializeField] private Vector2 m_brickSize = new Vector2(1, 0.5f);
+    [SerializeField] private GameObject m_brickPrefab;
+
+    private void Awake()
     {
-        
+        CreateBricks();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateBricks()
     {
-        
+        Vector2 centerPosition = transform.position;
+
+        Vector2 startOffset = new Vector2(
+            -(m_brickSize.x + m_padding.x) * (m_gridSize.x - 1) / 2f,
+            -(m_brickSize.y + m_padding.y) * (m_gridSize.y - 1) / 2f
+        );
+
+        for (int i = 0; i < m_gridSize.x; i++)
+        {
+            for (int j = 0; j < m_gridSize.y; j++)
+            {
+                Vector2 brickPosition = centerPosition + startOffset +
+                    new Vector2(
+                        (m_brickSize.x + m_padding.x) * i,
+                        (m_brickSize.y + m_padding.y) * j
+                    );
+
+                Instantiate(m_brickPrefab, brickPosition, Quaternion.identity);
+            }
+        }
     }
+
 }
